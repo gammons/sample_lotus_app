259ca1cfbefdb55418cc02eff847475549df9afa
Grant Ammons <gammons@gmail.com>
2014-05-16 20:47:37 -0400

    remove comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require 'application'
 
 Bundler.require(:default, :test)
 
-#require 'minitest'
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'lotus/model/adapters/memory_adapter'
@@ -14,36 +13,3 @@ adapter = Application.setup_adapter do
   type     Lotus::Model::Adapters::MemoryAdapter
   mapper   :default
 end
-
-# module RSpec
-#   module FeatureExampleGroup
-#     def self.included(group)
-#       group.metadata[:type] = :feature
-#       Capybara.app = Rack::Builder.new do
-#         use Rack::Session::Cookie, secret: 'foo'
-#         use Rack::MethodOverride
-#         run Application
-#       end
-#     end
-#   end
-# end
-
-# RSpec.configure do |c|
-#   def c.escaped_path(*parts)
-#     Regexp.compile(parts.join('[\\\/]') + '[\\\/]')
-#   end
-#
-#   c.color = true
-#
-#   c.include RSpec::FeatureExampleGroup, type: :feature, example_group: {
-#     file_path: c.escaped_path(%w[spec features])
-#   }
-#
-#   c.include Capybara::DSL, type: :feature
-#
-#   c.before do
-#     Application.mapper(:default).collections.each do |collection|
-#       adapter.clear collection.first
-#     end
-#   end
-# end
