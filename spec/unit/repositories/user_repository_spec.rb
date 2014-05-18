30003a8fffe5a00e1b91dd6cc0f1ef09d784796b
Grant Ammons <gammons@gmail.com>
2014-05-17 21:20:52 -0400

    update specs for user

--- spec/unit/repositories/user_repository_spec.rb
+++ spec/unit/repositories/user_repository_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 describe UserRepository do
+  describe "creating a user" do
+    let(:user) { User.new(name: 'bob') }
+
+    it "sets a remember token" do
+      u = UserRepository.create(user)
+      u.remember_token.wont_be_nil
+    end
+
+    it "sets created_at and updated_at" do
+      u = UserRepository.create(user)
+      u.created_at.wont_be_nil
+      u.updated_at.wont_be_nil
+    end
+  end
 
 end
