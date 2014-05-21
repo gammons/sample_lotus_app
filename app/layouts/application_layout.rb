82b6dfa219dfd97b16ab9f2c7207606ff38dd669
Grant Ammons <gammons@gmail.com>
2014-05-20 22:02:37 -0400

    get full_title working

--- app/layouts/application_layout.rb
+++ app/layouts/application_layout.rb
@@ -1,4 +1,17 @@
 class ApplicationLayout
   include Lotus::Layout
+
+  def full_title
+    base_title = "App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def page_title
+    "Main"
+  end
 end
 
